6a120ba448abb855f17a9f881e5f228c5385ad13
AChentsova <anjaisliss@gmail.com>
2026-02-01 22:05:21 +0200

    Add registration form tests and update package.json scripts

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,28 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+/// <reference types="cypress" />
+
+Cypress.Commands.add("login", (email, password) => {
+  cy.visit("https://qauto.forstudy.space", {
+    auth: {
+      username: Cypress.env("LOGIN_USERNAME"),
+      password: Cypress.env("LOGIN_PASSWORD"),
+    },
+  });
+  cy.get(".btn.btn-outline-white.header_signin").click();
+  cy.get("#signinEmail").type(email);
+  cy.get("#signinPassword").type(password);
+  cy.contains(".btn.btn-primary", "Login").click();
+});
+
+
+Cypress.Commands.overwrite("type", (originalFn, element, text, options) => {
+  if (options && options.sensitive) {
+    options.log = false;
+    Cypress.log({
+      $el: element,
+      name: "type",
+      message: "*".repeat(text.length),
+    });
+  }
+
+  return originalFn(element, text, options);
+});
